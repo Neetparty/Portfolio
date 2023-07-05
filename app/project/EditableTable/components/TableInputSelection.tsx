31b69f839a2fcef95989c94447ea679c403e08a5
Hitorin <phantomstudio299@gmail.com>
2023-07-05 15:12:19 +0700

    Fix API

--- app/project/EditableTable/components/TableInputSelection.tsx
+++ app/project/EditableTable/components/TableInputSelection.tsx
@@ -1,22 +1,20 @@
-'use client'
+'use server'
 
+import axios from "axios"
 import { use } from "react"
 
 const getData = async () => {
 
-    const res = await fetch('https://neetparty.github.io/Portfolio/api/selection')
-
-    if (!res.ok) {
-        throw new Error('Failed to fetch data')
-    }
-
-    return res.json()
+    const res = await axios.get('https://neetparty.github.io/Portfolio/api/selection')
+    return res.data
 }
 
 const dataPromise = getData()
 
 export const TableInputSelection = () => {
-    const data = use(dataPromise)
+
+    const raw = use(dataPromise)
+    const data = raw.DATA
 
     return (
         data.DATA.map((a: { name_en: string, name_th: string }, i: number) => <option key={i} value={a.name_en}>{a.name_en}</option> )
