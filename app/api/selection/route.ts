6d544faf0759392c903ac93c8dade273640f24c0
Hitorin <phantomstudio299@gmail.com>
2023-07-05 16:08:52 +0700

    Test new config

--- app/api/selection/route.ts
+++ app/api/selection/route.ts
@@ -3,5 +3,12 @@ import rawData from '../data/artwork.json'
 
 export async function GET(request: Request, ) {
 
-    return NextResponse.json(rawData)
+    return NextResponse.json(rawData, {
+        status: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        }
+      })
 }
