7681b6b7292f5710678bf2c32c9e341f61b49832
Hitorin <phantomstudio299@gmail.com>
2023-07-05 16:13:08 +0700

    Fix fetch data

--- app/project/EditableTable/components/TableInputSelection.tsx
+++ app/project/EditableTable/components/TableInputSelection.tsx
@@ -1,19 +1,23 @@
 
-import axios from "axios"
 import { use } from "react"
 
 const getData = async () => {
 
-    const res = await axios.get('https://neetparty.github.io/Portfolio/api/selection')
-    return res.data
+    const res = await fetch('https://neetparty.github.io/Portfolio/api/selection')
+
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
 }
 
 const dataPromise = getData()
 
 export const TableInputSelection = () => {
 
-    const raw = use(dataPromise)
-    const data = raw.DATA
+    const data = use(dataPromise)
 
     return (
         data.DATA.map((a: { name_en: string, name_th: string }, i: number) => <option key={i} value={a.name_en}>{a.name_en}</option> )
